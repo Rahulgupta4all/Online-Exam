59cacbd03d504209fc57a1466c5fe4805b9fb8a2
Rahulgupta4all <51700794+Rahulgupta4all@users.noreply.github.com>
2020-05-18 11:50:21 +0530

    Add files via upload

---
+++ onineExam/scripts/utils/firebase.js
@@ -0,0 +1,47 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBBDkw6p4KkMTY6N_W0ZXxTjBNp-3LILD0",
+  authDomain: "online-exam-c900e.firebaseapp.com",
+  databaseURL: "https://online-exam-c900e.firebaseio.com",
+  projectId: "online-exam-c900e",
+  storageBucket: "online-exam-c900e.appspot.com",
+  messagingSenderId: "557309212758",
+  appId: "1:557309212758:web:07a9c899e5527d1c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyCRy4QhPxtxmilqoMV36WKTnjeBOxXvGFY",
+//   authDomain: "onlineexam-40ead.firebaseapp.com",
+//   databaseURL: "https://onlineexam-40ead.firebaseio.com",
+//   projectId: "onlineexam-40ead",
+//   storageBucket: "",
+//   messagingSenderId: "1033541696497",
+//   appId: "1:1033541696497:web:45349c85773e33ae"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyAma1nF4GKkCzKpOhO9I0ix5WUlIEIdPlA",
+//   authDomain: "onlone.firebaseapp.com",
+//   databaseURL: "https://onlone.firebaseio.com",
+//   projectId: "onlone",
+//   storageBucket: "onlone.appspot.com",
+//   messagingSenderId: "233143261505",
+//   appId: "1:233143261505:web:4374f84213b8c31418a9c0"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+
